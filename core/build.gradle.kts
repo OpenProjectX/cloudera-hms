4a65218769cda52273dcb118c2c10361a2cd3677
CoderYellow <CoderYellow@hotmail.com>
2026-04-06 17:23:38 +0800

    refactor(build): remove commented-out dependency exclusions in core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,42 +3,42 @@ plugins {
 }
 
 configurations.configureEach {
-    exclude(group = "org.apache.zookeeper")
-    exclude(group = "org.apache.hadoop", module = "hadoop-yarn-server-resourcemanager")
-    exclude(group = "org.eclipse.jetty")
-    exclude(group = "org.apache.tez")
-    exclude(group = "org.apache.curator")
-    exclude(group = "org.apache.twill")
-    exclude(group = "org.apache.parquet", module = "parquet-hadoop-bundle")
+//    exclude(group = "org.apache.zookeeper")
+//    exclude(group = "org.apache.hadoop", module = "hadoop-yarn-server-resourcemanager")
+//    exclude(group = "org.eclipse.jetty")
+//    exclude(group = "org.apache.tez")
+//    exclude(group = "org.apache.curator")
+//    exclude(group = "org.apache.twill")
+//    exclude(group = "org.apache.parquet", module = "parquet-hadoop-bundle")
 
-    listOf(
-        "org.apache.atlas",
-        "com.sun.jersey",
-        "org.apache.calcite.avatica",
-        "org.apache.calcite",
-        "junit",
-        "co.cask.tephra",
-        "org.apache.ivy",
-        "org.apache.ant",
-        "org.codehaus.groovy",
-        "javax.servlet",
-        "commons-cli",
-        "org.apache.orc",
-    )
-        .forEach { exclude(group = it) }
+//    listOf(
+//        "org.apache.atlas",
+//        "com.sun.jersey",
+//        "org.apache.calcite.avatica",
+//        "org.apache.calcite",
+//        "junit",
+//        "co.cask.tephra",
+//        "org.apache.ivy",
+//        "org.apache.ant",
+//        "org.codehaus.groovy",
+//        "javax.servlet",
+//        "commons-cli",
+//        "org.apache.orc",
+//    )
+//        .forEach { exclude(group = it) }
 
-    listOf(
-        "org.apache.hive" to "hive-llap-tez",
-        "org.apache.hadoop" to "hadoop-yarn-registry",
-        "org.apache.hadoop" to "hadoop-yarn-api",
-        "org.apache.hive" to "hive-vector-code-gen",
-        "org.apache.hive" to "hive-shims",
-
-
-        )
-        .forEach { (group, module) ->
-            exclude(group = group, module = module)
-        }
+//    listOf(
+//        "org.apache.hive" to "hive-llap-tez",
+//        "org.apache.hadoop" to "hadoop-yarn-registry",
+//        "org.apache.hadoop" to "hadoop-yarn-api",
+//        "org.apache.hive" to "hive-vector-code-gen",
+//        "org.apache.hive" to "hive-shims",
+//
+//
+//        )
+//        .forEach { (group, module) ->
+//            exclude(group = group, module = module)
+//        }
 
 }
 
